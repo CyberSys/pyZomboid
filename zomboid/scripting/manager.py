02f9ab16c5e3258f82d70490ba5c0650ca3c0cab
FWolfe <noreply@tekagis.ca>
2020-08-20 07:42:07 -0700

    hints and addition attributes

--- zomboid/scripting/manager.py
+++ zomboid/scripting/manager.py
@@ -11,15 +11,24 @@ import re
 logger = logging.getLogger(__name__)
 
 RE_MODULE = re.compile(r"module\s+([^{]+?)\s*{(.*)}", flags=re.DOTALL | re.M)
+from zomboid.java import ArrayList
 
 from .module import ScriptModule, ModuleMap
 from .parser import ScriptParser
 
 
 class ScriptManager:
+    currentFileName : str = None
+    scriptsWithVehicles : ArrayList = None
+    scriptsWithVehicleTemplate : ArrayList = None
+    
+    def __init__(self):
+        self.scriptsWithVehicles = ArrayList()
+        self.scriptsWithVehicleTemplate = ArrayList()
+
 
     @classmethod
-    def FindItem(cls, full_type):
+    def FindItem(cls, full_type : str): # -> scripting.objects.item.Item
         try:
             module, item_type = full_type.split('.',1)
             return ModuleMap[module].items[item_type]
@@ -31,26 +40,29 @@ class ScriptManager:
 
 
     @classmethod
-    def ParseScript(cls, lines):
+    def ParseScript(cls, lines : str) -> None:
         tokens = ScriptParser.parseTokens(lines)
         for token in tokens:
             cls.CreateFromToken(token)
 
         
     @classmethod
-    def LoadFile(cls, filename, boolean):
+    def LoadFile(cls, filename : str, boolean : bool) -> None:
         this = open(filename)
         lines = this.read()
         this.close()
         lines = ScriptParser.stripComments(lines)
+        cls.currentFileName = filename # this is bad. shouldnt be a class method anymore
         cls.ParseScript(lines)
+        cls.currentFileName = None
 
 
     @classmethod
-    def CreateFromToken(cls, data):
+    def CreateFromToken(cls, data : str) -> None:
         match = RE_MODULE.match(data)
         if not match:
             return
+
         module_name = match.group(1)
         data = match.group(2)
         if module_name not in ModuleMap:
