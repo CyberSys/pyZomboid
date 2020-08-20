80ccc5cd68ad3ad5b33b6d0d27c23da133e38f7b
FWolfe <noreply@tekagis.ca>
2020-08-20 07:38:24 -0700

    finish BaseScriptObject

--- zomboid/scripting/objects/base.py
+++ zomboid/scripting/objects/base.py
@@ -1,3 +1,9 @@
 class BaseScriptObject:
+    module = None # cant use hints due to import recursion. ..module.ScriptModue
+
     def Load(self, name, data):
         pass
+
+    def getModule(self):
+        return self.module
+
